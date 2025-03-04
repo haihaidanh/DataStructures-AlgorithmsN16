bfc5a95def4d8b61190368463521c8a66a440dc3
haihaidanh <danhhai27@gmail.com>
2025-03-04 10:16:40 +0700

    Update insertion_sort.cpp

--- BaiTapTrenLop/04032025/insertion_sort.cpp
+++ BaiTapTrenLop/04032025/insertion_sort.cpp
@@ -13,33 +13,7 @@ void insertion_sort(int a[],int n){
 	
 }
 
-//void bubble_sort(int a[],int n){
-//	for(int i=0;i<n;i++){
-//		for(int j=i+1;j<n;j++){
-//			if(a[j]<a[i]) swap(a[i],a[j]);
-//		}
-//	}
-//}
-//
-//void selection_sort(int a[],int n){
-//	for(int i=0;i<n-1;i++){
-//		int min=i;
-//		for(int j=i+1;j<n;j++){
-//			if(a[j]<a[min]) min=j;
-//		}
-//		swap(a[min],a[i]);
-//	}
-//}
-//
-//void so(int a[],int n){
-//	selection_sort(a,n);
-//	int l=0,r=n-1;
-//	while(l<r){
-//		cout<<a[r]<<" "<<a[l]<<" ";
-//		l++,r--;
-//	}
-//	if(n%2==1) cout<<a[l];
-//}
+
 int main(){
 	int t=1;
 	while(t--){
