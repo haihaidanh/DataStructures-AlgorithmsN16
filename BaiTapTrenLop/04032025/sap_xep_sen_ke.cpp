975c20cb492be29841270c1093d045a9179275df
haihaidanh <danhhai27@gmail.com>
2025-03-04 10:00:24 +0700

    Add files via upload

---
+++ BaiTapTrenLop/04032025/sap_xep_sen_ke.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//void sort(int a[],int n){
+//	for(int i=1;i<n;i++){
+//		int x=a[i],p=i-1;
+//		while(p>-1&&a[p]>x){
+//			
+//			a[p+1]=a[p];p--;
+//		}
+//		a[p+1]=x;
+//	}
+//	
+//}
+void sort(int a[],int n){
+	for(int i=0;i<n-1;i++){
+		int min=i;
+		for(int j=i+1;j<n;j++){
+			if(a[j]<a[min]) min=j;
+		}
+		swap(a[min],a[i]);
+	}
+}
+
+void so(int a[],int n){
+	sort(a,n);
+	int l=0,r=n-1;
+	while(l<r){
+		cout<<a[r]<<" "<<a[l]<<" ";
+		l++,r--;
+	}
+	if(n%2==1) cout<<a[l];
+}
+int main(){
+	int t;cin>>t;
+	while(t--){
+		int n;cin>>n;
+		int a[n];
+		for(int &x : a) cin>>x;
+		so(a,n);cout<<endl;
+	}
+	
+
+
+
+}
